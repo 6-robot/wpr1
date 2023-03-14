b7d59af1a0a9dcb88e08f7cccbee0437d7798664
zhangwanjie <zhangwanjie@126.com>
2023-03-14 22:19:38 +0800

    update for noetic

---
+++ wpr1_bringup/scripts/install_for_noetic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install ros-noetic-joint-state-publisher-gui
+sudo apt-get install ros-noetic-joy
+sudo apt-get install ros-noetic-hector-mapping
+sudo apt-get install ros-noetic-gmapping
+sudo apt-get install ros-noetic-navigation
+sudo apt-get install ros-noetic-cv-bridge
+sudo apt-get install ros-noetic-audio-common
+sudo apt-get install ros-noetic-controller-manager
+sudo apt-get install ros-noetic-urg-node
